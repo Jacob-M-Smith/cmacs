a94d2096ac5ef645adacebe2f9e3a438b585bf9e
Jacob M. Smith <jmsmith6101@att.net>
2023-11-08 00:49:23 -0500

    wrote function to handle file opening procedure

--- cmacs.c
+++ cmacs.c
@@ -2,27 +2,27 @@
 #include <stdlib.h>
 #include <ncurses.h>
 #include <errno.h>
+#include <sys/stat.h>
+#include "cmacs.h"
+
+char** buffer_list;
+FILE** open_file_list;
+uint buffer_list_size = 0;                 // open number of buffers should always 
+                                           // equal number of open files
+uint current_buffer = 0;
+
+// ^^^^^^^^^^^^^^
+//consider moving globals to main
 
 int main (int argc, char** argv)
 {
-    FILE* fd;
-    char buffer[1024];
-
     char* name = argv[1];
 
-    fd = fopen(name, "r");
-    
-    if (fd == NULL)
-    {
-        printf("error number %d\n", errno);
-        perror("cmacs");
-        return 0;
-    }
-
-    fread(buffer, 1024, 1, fd);
+    open_file(name);
+    // screen loop goes here
 
     initscr();
-    addstr(buffer);
+    addstr(buffer_list[current_buffer]);
 
     refresh();
 
@@ -32,24 +32,89 @@ int main (int argc, char** argv)
     getch();
     endwin();
 
-    fclose(fd);
+    close_all_files();
+
     return 1;
 }
 
-
-char* read_from_file(char* file_name)
+// should this return int for errors?
+// passed prelim testing (need in depth testing)
+void open_file(char* name)
 {
-    // will read data from file and store in a text buffer
-    // buffer needs to be accessible outside of function
-    // we also need to maintain a list of file descritpors to close later 
-    // need to hold a list of buffers to free later also
+    FILE* fd;
+    off_t file_size;
+    fd = fopen(name, "r+");
+
+    if (fd == NULL)
+    {
+        printf("error number %d\n", errno);
+        perror("cmacs failed to open file");
+        return;
+    }
+
+    struct stat stat_buffer;
+    if (stat(name, &stat_buffer) == -1)
+    {
+        printf("error number %d\n", errno);
+        perror("cmacs could not retrieve file data");
+        return;
+    }
+
+    file_size = *&stat_buffer.st_size;
+    char* buffer;
+
+    if ((buffer = (char*)malloc(sizeof(char) * file_size)) == NULL)
+    {
+        printf("failed to allocate memory\n");
+        fclose(fd);
+        return;
+    }
+
+    fread(buffer, file_size, 1, fd);
+
+    if (buffer_list_size == 0)
+    {
+        if (((open_file_list = (FILE**)malloc(sizeof(FILE*))) == NULL)
+             || (buffer_list = (char**)malloc(sizeof(char*))) == NULL)
+        {
+            printf("failed to allocate memory\n");
+            fclose(fd);
+            free(buffer);
+            return;
+        }        
+    }
+    else
+    {
+        if ((open_file_list = (FILE**)malloc(sizeof(FILE*) * (buffer_list_size + 1))) == NULL || ((buffer_list = (char**)malloc(sizeof(char*) * (buffer_list_size + 1))) == NULL))
+        {
+            printf("failed to allocate memory\n");
+            fclose(fd);
+            free(buffer);
+            return;
+        }                
+    }
+
+   buffer_list[buffer_list_size] = buffer;
+   open_file_list[buffer_list_size] = fd;
+   current_buffer = buffer_list_size;
+   buffer_list_size++;
 }
+
+
+void close_all_files()
+{
+    for (int i = 0; i < buffer_list_size; i++)
+    {
+        fclose(open_file_list[i]);
+        free(buffer_list[i]);
+    }
+}
+
 
 
 // FUNCTION RESPONSIBILITIES
 
 // Main
-// track current buffer
 // handle curses
 // make scr editable
 // save edits to buffer
@@ -67,9 +132,14 @@ char* read_from_file(char* file_name)
 // Update File
 // call when trap for saving is activated
 // take active buffer and write it to file
+// if file does not exist then create it 
 
 // Shutdown 
 // on exit
 // close all open files
 // free all allocated buffers
 
+// Switch current buffer
+// search files for correct name 
+// update buffer number
+// create new buffer on unrecognized name
