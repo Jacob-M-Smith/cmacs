7544882befcb4d867c039569db0fd267b0b76e14
Jacob M. Smith <jmsmith6101@att.net>
2023-11-27 20:12:43 -0500

    correctly insert character to middle of buffer

--- buffer.c
+++ buffer.c
@@ -12,8 +12,6 @@ int add_char_to_buffer(buffer* buf, char character)
     int buffer_size = buf->size;
     int pos = buf->pos;
 
-//    printf("pos: %d, n_len: %d\n", pos, new_text_len);
-
     if ((new_text_len - 1) >= buffer_size)
     {
         if ((buf->text = (char*)realloc(buf->text, buffer_size + 1024)) == NULL)
@@ -22,19 +20,16 @@ int add_char_to_buffer(buffer* buf, char character)
             return 0;
         }
         buf->size += 1024;
-//        printf("(realloc o_sz: %d, n_sz: %d\n)", buffer_size, buf->size);
     }
 
     if (pos != (new_text_len - 1))                                               // This means we are not inserting at the end of the char* and need to shift the characters.
     {        
-//        printf("middle \n");
-        memmove(&buf->text[pos], &buf->text[pos + 1], strlen(&buf->text[pos]));
+        memcpy(buf->text + pos + 1, buf->text + pos, (buf->text - pos)*sizeof(char));
         buf->text[pos] = character;
         return 1;
     }
     else
     {
-//        printf("end pos: %d\n", pos);
         buf->text[pos] = character;
         buf->text[pos + 1] = '\0';
         return 1;
