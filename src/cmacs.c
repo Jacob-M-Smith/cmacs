eb8f2648907864495a678aa9e8d5bd80167aec32
Jacob M. Smith <jmsmith6101@att.net>
2024-03-07 19:13:03 -0500

    Driver calls sub menu function when it recieves the signal for ctrl + x rather than handling it itself.

--- src/cmacs.c
+++ src/cmacs.c
@@ -35,18 +35,9 @@ int main (int argc, char** argv)
         {
             key = getch();
             
-            if (key == CTRL('s'))            
-                update_file();
-            else if (key == CTRL('c'))
+            if (key == CTRL('c'))
                 break;
-            else
-            {
-                // go to ctrl+x sub menu when implemented
-
-                // if key ==  27 go to alt menu and read another code 
-                // if there is no other code (esc) exit 
-                continue;
-            }
+            ctrl_x_sub_commands(key); 
         }
         else
         {           
